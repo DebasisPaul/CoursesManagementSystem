8231ba7713d0abacc39f0a58a090f50e5f2305af
debasispaul <idebasispaul@gmail.com>
2022-09-11 12:05:17 -0700

    project updated

--- TechTreeMVCWebApplication/Controllers/ContentController.cs
+++ TechTreeMVCWebApplication/Controllers/ContentController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TechTreeMVCWebApplication.Data;
 using TechTreeMVCWebApplication.Entities;
@@ -17,13 +20,14 @@ namespace TechTreeMVCWebApplication.Controllers
         public async Task<IActionResult> Index(int categoryItemId)
         {
             Content content = await (from item in _context.Content
-                                    where item.CategoryItem.Id == categoryItemId
-                                    select new Content
-                                    {
-                                        Title = item.Title,
-                                        VideoLink = item.VideoLink,
-                                        HTMLContent = item.HTMLContent
-                                    }).FirstOrDefaultAsync();
+                                     where item.CategoryItem.Id == categoryItemId
+                                     select new Content
+                                     {
+                                         Title = item.Title,
+                                         VideoLink = item.VideoLink,
+                                         HTMLContent = item.HTMLContent
+                                     }).FirstOrDefaultAsync();
+
             return View(content);
         }
     }
